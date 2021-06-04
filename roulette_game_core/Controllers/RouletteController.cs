256da05a92fb392166c062086ea401f6a81827ca
oscarporras02 <oscarrussi@outlook.com>
2021-06-04 11:35:10 -0500

    add endpoint to get all the roulettes and their status

--- roulette_game_core/Controllers/RouletteController.cs
+++ roulette_game_core/Controllers/RouletteController.cs
@@ -12,9 +12,24 @@ namespace roulette_game_core.Controllers
     [ApiController]
     public class RouletteController : ControllerBase
     {
+        // endpoint to list all the roulettes and their status
+        // if winnerNumber=-1 then the roulette is still open, otherwise it's already closed
+        [HttpGet]
+        public IEnumerable<Object> Get()
+        {
+            using (var context = new masivianContext())
+            {
+                return context.Roulettes.ToList<Roulette>().Select(nRoulette => new
+                {
+                    nRoulette.Id,
+                    Status= nRoulette.WinnerNumber == -1 ? "Open" : "Close"
+                });
+            }
+        }
+
         // endpoint to create a new roulette
         [HttpPost]
-        public Object Post()
+        public int Post()
         {
             using (var context = new masivianContext())
             {
