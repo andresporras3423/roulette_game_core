c52be50b6bffbb682d2fa358200b540a611e35ec
oscarporras02 <oscarrussi@outlook.com>
2021-06-04 11:07:50 -0500

    add endpoint to create a roulette

---
+++ roulette_game_core/Controllers/RouletteController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using roulette_game_core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace roulette_game_core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RouletteController : ControllerBase
+    {
+        // endpoint to create a new roulette
+        [HttpPost]
+        public Object Post()
+        {
+            using (var context = new masivianContext())
+            {
+                Roulette nRoulette = new Roulette();
+                context.Roulettes.Add(nRoulette);
+                context.SaveChanges();
+                return nRoulette.Id;
+            }
+        }
+    }
+}
