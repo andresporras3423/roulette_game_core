e9260149b51149572902faa6b61d71f343ac15b1
oscarporras02 <oscarrussi@outlook.com>
2021-06-05 12:03:22 -0500

    publish with azure

--- roulette_game_core/obj/Release/netcoreapp3.1/roulette_game_core.AssemblyInfo.cs
+++ roulette_game_core/obj/Release/netcoreapp3.1/roulette_game_core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("1fbcaedd-e71b-4b69-8963-aa28c84913a5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("roulette_game_core")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("roulette_game_core")]
