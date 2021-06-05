921e955acbade2e02d7e4cdc812af7faab92e3c6
oscarporras02 <oscarrussi@outlook.com>
2021-06-04 21:01:57 -0500

    add endpoint for close a bet

--- roulette_game_core/Controllers/RouletteController.cs
+++ roulette_game_core/Controllers/RouletteController.cs
@@ -63,5 +63,55 @@ namespace roulette_game_core.Controllers
                 return StatusCode(400);
             }
         }
+
+        [HttpPut]
+        [Route("close/{id}")]
+        public object Close(int id)
+        {
+            try
+            {
+                using (var context = new masivianContext())
+                {
+                    Roulette nRoulette = context.Roulettes.Find(id);
+                    if (nRoulette.RouletteStatusId == 2)
+                    {
+                        nRoulette.RouletteStatusId = 3;
+                        context.SaveChanges();
+                        Random rd = new Random();
+                        int rand_num = rd.Next(0, 36);
+                        return (from rb in context.RouletteBets
+                                where rb.RouletteId == id
+                                select new betOutput(rb.PlayerId, rb.Bet, rb.BetValue, rand_num).displayContent())
+                                .ToList();
+                    }
+                    else return new { error = "La ruleta no está abierta" };
+                }
+            }
+            catch
+            {
+                return new { error = "La ruleta con el id especificado no existe" };
+            }
+        }
+
+        private class betOutput{
+            int PlayerId;
+            string Bet;
+            string BetStatus;
+            double BetValue;
+            double BetPrize;
+            public betOutput(int PlayerId, string Bet, double BetValue, int rand)
+            {
+                this.PlayerId = PlayerId;
+                this.Bet = Bet;
+                this.BetValue = BetValue;
+                this.BetStatus = (Bet == "rojo" && rand % 2 == 0) || (Bet == "negro" && rand % 2 == 1) || (Int32.Parse(Bet) == rand) ? "ganador" : "perdedor";
+                this.BetPrize = (this.BetStatus == "perdedor" ? 0.0 : (Bet=="rojo" || Bet=="negro" ? BetValue*1.8 : BetValue*5));
+            }
+
+            public object displayContent()
+            {
+                return new { this.PlayerId, this.Bet, this.BetValue, this.BetStatus, this.BetPrize };
+            }
+        };
     }
 }
